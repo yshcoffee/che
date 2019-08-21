1ad5c7a73d0f2f1056d6e3086823e25364b4f9db
musienko maksym <mmusiien@redhat.com>
2019-08-21 13:40:44 +0300

    increase timeout for java LS

--- e2e/tests/e2e_happy_path/HappyPath.spec.ts
+++ e2e/tests/e2e_happy_path/HappyPath.spec.ts
@@ -88,7 +88,7 @@ suite('Language server validation', async () => {
         await projectTree.expandPathAndOpenFileInAssociatedWorkspace(pathToJavaFolder, javaFileName);
         await editor.selectTab(javaFileName);
         await ide.checkLsInitializationStart('Starting Java Language Server');
-        await ide.waitStatusBarTextAbsence('Starting Java Language Server', 600000);
+        await ide.waitStatusBarTextAbsence('Starting Java Language Server', 900000);
         await checkJavaPathCompletion();
         await ide.waitStatusBarTextAbsence('Building workspace', 360000);
     });
